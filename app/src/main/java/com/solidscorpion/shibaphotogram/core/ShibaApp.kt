82044c5954a43cf74690a4ce3bd14a0bb9b958b4
Anton Pripachkin <>
2018-11-26 15:46:09 +0200

    Added libraries

---
+++ app/src/main/java/com/solidscorpion/shibaphotogram/core/ShibaApp.kt
@@ -0,0 +1,9 @@
+package com.solidscorpion.shibaphotogram.core
+
+import android.app.Application
+
+class ShibaApp : Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
